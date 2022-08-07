4dcaf91ee3456f8aa1b1e464f27c7f9e8388c8f4
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2022-08-07 12:49:34 +0200

    inject doesn't work outside dev env

--- src/main/java/cc/polyfrost/polytime/mixin/WorldInfoMixin.java
+++ src/main/java/cc/polyfrost/polytime/mixin/WorldInfoMixin.java
@@ -3,6 +3,8 @@ package cc.polyfrost.polytime.mixin;
 import cc.polyfrost.polytime.PolyTime;
 import net.minecraft.world.storage.WorldInfo;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,10 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(WorldInfo.class)
 public class WorldInfoMixin {
 
-    @Inject(method = "getWorldTime", at = @At("HEAD"), cancellable = true)
-    private void getWorldTime(CallbackInfoReturnable<Long> cir) {
-        if (PolyTime.INSTANCE.config.enabled) {
-            cir.setReturnValue(PolyTime.timeToTicks());
-        }
+    @Shadow private long worldTime;
+
+    @Overwrite
+    public long getWorldTime() {
+        if (PolyTime.INSTANCE.config.enabled) return PolyTime.timeToTicks();
+        return this.worldTime;
     }
 }
