c5a6221b92c3d9d3b9b7fe51c4a5754272d62120
Wyvest <wyvestbusiness@gmail.com>
2023-05-31 10:09:54 -0400

    add credit to fyu

--- src/main/java/cc/polyfrost/polytime/config/TimeConfig.java
+++ src/main/java/cc/polyfrost/polytime/config/TimeConfig.java
@@ -2,12 +2,20 @@ package cc.polyfrost.polytime.config;
 
 import cc.polyfrost.oneconfig.config.Config;
 import cc.polyfrost.oneconfig.config.annotations.Checkbox;
+import cc.polyfrost.oneconfig.config.annotations.Info;
 import cc.polyfrost.oneconfig.config.annotations.Slider;
+import cc.polyfrost.oneconfig.config.data.InfoType;
 import cc.polyfrost.oneconfig.config.data.Mod;
 import cc.polyfrost.oneconfig.config.data.ModType;
 import cc.polyfrost.polytime.PolyTime;
 
 public class TimeConfig extends Config {
+
+    @Info(
+            text = "Credits to Fyu for the original TimeChanger mod.",
+            type = InfoType.INFO
+    )
+    private static boolean credits;
 
     @Slider(
             name = "Time",
