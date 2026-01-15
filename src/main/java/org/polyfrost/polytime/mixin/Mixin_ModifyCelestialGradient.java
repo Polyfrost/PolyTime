1d656da9b544ed7db7cf5ed3d0a1671dfbb6c285
MicrocontrollersDev <microcontrollersyt@gmail.com>
2026-01-14 22:33:50 -0700

    1.21 stonecutter

--- src/main/java/org/polyfrost/polytime/mixin/Mixin_ModifyCelestialGradient.java
+++ src/main/java/org/polyfrost/polytime/mixin/Mixin_ModifyCelestialGradient.java
@@ -1,29 +1,33 @@
 package org.polyfrost.polytime.mixin;
 
-import net.minecraft.client.multiplayer.WorldClient;
-import net.minecraft.profiler.Profiler;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.storage.ISaveHandler;
-import net.minecraft.world.storage.WorldInfo;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.storage.WritableLevelData;
 import org.polyfrost.polytime.client.PolyTimeClient;
 import org.polyfrost.polytime.client.PolyTimeConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
-@Mixin(WorldClient.class)
-public abstract class Mixin_ModifyCelestialGradient extends World {
-    protected Mixin_ModifyCelestialGradient(ISaveHandler saveHandlerIn, WorldInfo info, WorldProvider providerIn, Profiler profilerIn, boolean client) {
-        super(saveHandlerIn, info, providerIn, profilerIn, client);
+import java.util.function.Supplier;
+
+@Mixin(ClientLevel.class)
+public abstract class Mixin_ModifyCelestialGradient extends Level {
+    protected Mixin_ModifyCelestialGradient(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, /*? if 1.21.1 {*/ /*Supplier<ProfilerFiller> profiler, *//*?}*/ boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
+        super(levelData, dimension, registryAccess, dimensionTypeRegistration, /*? if 1.21.1 {*/ /*profiler, *//*?}*/ isClientSide, isDebug, biomeZoomSeed, maxChainedNeighborUpdates);
     }
 
     @Override
-    public float getCelestialAngle(float tickDelta) {
+    public float getTimeOfDay(float tickDelta) {
         if (PolyTimeConfig.isEnabled()) {
             return polyweather$angleFromTime(PolyTimeClient.getCurrentTime(), tickDelta);
         }
 
-        return super.getCelestialAngle(tickDelta);
+        return super.getTimeOfDay(tickDelta);
     }
 
     @Unique
--- src/main/kotlin/org/polyfrost/polytime/PolyTimeConstants.kt
+++ src/main/kotlin/org/polyfrost/polytime/PolyTimeConstants.kt
@@ -2,7 +2,10 @@ package org.polyfrost.polytime
 
 object PolyTimeConstants {
     // Sets the variables from `gradle.properties`. Depends on the `bloom` DGT plugin.
-    const val ID: String = "@MOD_ID@"
-    const val NAME: String = "@MOD_NAME@"
-    const val VERSION: String = "@MOD_VERSION@"
+//    const val ID: String = "@MOD_ID@"
+//    const val NAME: String = "@MOD_NAME@"
+//    const val VERSION: String = "@MOD_VERSION@"
+    const val ID: String = "polytime"
+    const val NAME: String = "PolyTime"
+    const val VERSION: String = "1.0.0-alpha.2"
 }
