732628f50bb4f16932a8d29ab62b6eb55026ba91
Saad Nadeem <saadndm.sn@gmail.com>
2026-07-01 11:17:17 -0400

    feat(ornithe): port mixins

--- src/main/java/org/polyfrost/polytime/mixin/Mixin_ModifyCelestialGradient.java
+++ src/main/java/org/polyfrost/polytime/mixin/Mixin_ModifyCelestialGradient.java
@@ -1,6 +1,7 @@
 package org.polyfrost.polytime.mixin;
 
 //? if < 1.21.11 {
+//? if > 1.8.9 {
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
@@ -10,6 +11,14 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.storage.WritableLevelData;
+//?} else {
+/*import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldData;
+import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.storage.WorldStorage;
+*///?}
 import org.polyfrost.polytime.client.PolyTimeClient;
 import org.polyfrost.polytime.client.PolyTimeConfig;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,11 +27,19 @@ import org.spongepowered.asm.mixin.Unique;
 //? if 1.21.1
 //import java.util.function.Supplier;
 
+//? if > 1.8.9 {
 @Mixin(ClientLevel.class)
 public abstract class Mixin_ModifyCelestialGradient extends Level {
     protected Mixin_ModifyCelestialGradient(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, /*? if 1.21.1 {*/ /*Supplier<ProfilerFiller> profiler, *//*?}*/ boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
         super(levelData, dimension, registryAccess, dimensionTypeRegistration, /*? if 1.21.1 {*/ /*profiler, *//*?}*/ isClientSide, isDebug, biomeZoomSeed, maxChainedNeighborUpdates);
     }
+//?} else {
+/*@Mixin(ClientWorld.class)
+public abstract class Mixin_ModifyCelestialGradient extends World {
+    protected Mixin_ModifyCelestialGradient(WorldStorage storage, WorldData data, Dimension dimension, Profiler profiler, boolean isClient) {
+        super(storage, data, dimension, profiler, isClient);
+    }
+*///?}
 
     @Override
     public float getTimeOfDay(float tickDelta) {
