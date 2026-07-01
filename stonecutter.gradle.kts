4552ffd633932f62e2c708960d77c6e66a4f885a
Saad Nadeem <saadndm.sn@gmail.com>
2026-07-01 10:46:07 -0400

    feat(ornithe): build setup

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,3 +3,19 @@ plugins {
 }
 
 stonecutter active "1.21.10" /* [SC] DO NOT EDIT */
+stonecutter {
+    parameters {
+        replacements {
+            string(eval(current.version, "= 1.8.9")) {
+                replace(
+                    "com.mojang.blaze3d.platform.InputConstants",
+                    "org.polyfrost.oneconfig.internal.legacy.InputConstants"
+                )
+                replace(
+                    "net.minecraft.server.Bootstrap",
+                    "net.minecraft.Bootstrap"
+                )
+            }
+        }
+    }
+}
