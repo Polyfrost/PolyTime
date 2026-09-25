88df54093e4ac34955cd0ea7ea5bdbe00fd88d57
Saad Nadeem <saadndm.sn@gmail.com>
2026-09-24 23:53:48 -0400

    feat(legacy): build setup

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -4,6 +4,19 @@ plugins {
 
 stonecutter active "26.3" /* [SC] DO NOT EDIT */
 
-stonecutter tasks {
-    order("publishModrinth")
+stonecutter {
+    tasks {
+        order("publishModrinth")
+    }
+
+    parameters {
+        replacements {
+            string(eval(current.version, "= 1.8.9")) {
+                replace(
+                    "com.mojang.blaze3d.platform.InputConstants",
+                    "org.polyfrost.oneconfig.internal.legacy.InputConstants"
+                )
+            }
+        }
+    }
 }
